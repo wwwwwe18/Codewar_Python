2340e54cb4f12c30e33e5e820bf841f24c37879f
wwwwwe18 <60565177+wwwwwe18@users.noreply.github.com>
2020-02-07 12:14:11 +0800

    Add files via upload

---
+++ who_likes_it.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Codewar: Who likes it?
+"""
+def likes(names):
+    num_likes = len(names)
+    #your code here
+    if(num_likes == 0):
+        print("no one likes this")
+    elif(num_likes == 1):
+        print(names[0] + " likes this")
+    elif(num_likes == 2):
+        print(names[0] + " and " + names[1] + " like this")
+    elif(num_likes == 3):
+        print(names[0] + ", " + names[1] + " and " + names[2] + " like this")
+    else:
+        print(names[0] + ", " + names[1] + " and " + str(num_likes-2) + " others like this")       
